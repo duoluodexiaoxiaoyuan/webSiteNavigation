209c3026b6a71c377f28d0823a216815ec7fae43
duoluodexiaoxiaoyuan <1203125214@qq.com>
2022-10-08 11:51:15 +0800

    网址导航

---
+++ ceshi/a.js
@@ -0,0 +1,58 @@
+let a = {
+  "default":[
+    {
+      "name": "鱼塘热榜",
+      "img":"https://blz.nosdn.127.net/sre/images/nav/news.png",
+      "description": "汇聚了数十个网站的热点信息"
+    },
+    {
+      "name": "知乎",
+      "img":"https://blz.nosdn.127.net/sre/images/nav/zhih.png",
+      "description": "知名问答社区"
+    },
+    {
+      "name": "鱼塘热榜",
+      "img":"https://blz.nosdn.127.net/sre/images/nav/news.png",
+      "description": "汇聚了数十个网站的热点信息"
+    },
+    {
+      "name": "知乎",
+      "img":"https://blz.nosdn.127.net/sre/images/nav/zhih.png",
+      "description": "知名问答社区"
+    },
+    {
+      "name": "鱼塘热榜",
+      "img":"https://blz.nosdn.127.net/sre/images/nav/news.png",
+      "description": "汇聚了数十个网站的热点信息"
+    },
+    {
+      "name": "知乎",
+      "img":"https://blz.nosdn.127.net/sre/images/nav/zhih.png",
+      "description": "知名问答社区"
+    },
+    {
+      "name": "鱼塘热榜",
+      "img":"https://blz.nosdn.127.net/sre/images/nav/news.png",
+      "description": "汇聚了数十个网站的热点信息"
+    },
+    {
+      "name": "知乎",
+      "img":"https://blz.nosdn.127.net/sre/images/nav/zhih.png",
+      "description": "知名问答社区"
+    },
+    {
+      "name": "鱼塘热榜",
+      "img":"https://blz.nosdn.127.net/sre/images/nav/news.png",
+      "description": "汇聚了数十个网站的热点信息"
+    },
+    {
+      "name": "知乎",
+      "img":"https://blz.nosdn.127.net/sre/images/nav/zhih.png",
+      "description": "知名问答社区"
+    }
+  ]
+}
+
+for(let key in a){
+  console.log();
+}
